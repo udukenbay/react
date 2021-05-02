32b86b0d6cd1ff39b44e46b4e4814eb0f64998a0
Urziya Dukenbay <62696463+udukenbay@users.noreply.github.com>
2021-05-02 07:58:32 +0600

    Add files via upload

---
+++ React exercise/Project-1/hello-world/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Intro from './Intro';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>Hey</p>
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ React exercise/Project-1/hello-world/src/Intro.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Hey Welcome to React. It is your first Component Example</h2>
+      </div>
+    );
+  }
+}
+
+export default Intro;
